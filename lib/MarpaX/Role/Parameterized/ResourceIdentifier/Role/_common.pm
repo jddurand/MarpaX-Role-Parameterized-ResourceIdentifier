ccfc3dc6c7156d6af5f20721d0df0bc5dd878fd3
Jean-Damien Durand <jeandamiendurand@free.fr>
2015-11-04 17:27:51 +0100

    dev+: build_percent_encoding_normalizer

--- lib/MarpaX/Role/Parameterized/ResourceIdentifier/Role/_common.pm
+++ lib/MarpaX/Role/Parameterized/ResourceIdentifier/Role/_common.pm
@@ -9,9 +9,13 @@ package MarpaX::Role::Parameterized::ResourceIdentifier::Role::_common;
 
 # AUTHORITY
 
+use Encode qw/encode/;
+use MarpaX::RFC::RFC3629;
 use Moo::Role;
+use MooX::Role::Logger;
 use Unicode::Normalize qw/normalize/;
 use Types::Standard -all;
+use Try::Tiny;
 #
 # Arguments of every callback:
 # my ($self, $field, $value, $lhs) = @_;
@@ -51,6 +55,74 @@ around build_character_normalizer => sub {
   # interoperability.
   #
   $rc->{''} = sub { normalize('C',  $_[2]) } if (! $self->is_character_normalized);
+  $rc
+};
+
+around build_percent_encoding_normalizer => sub {
+  my ($orig, $self) = (shift, shift);
+  my $rc = $self->$orig(@_);
+  #
+  # --------------------------------------------
+  # http://tools.ietf.org/html/rfc3987
+  # --------------------------------------------
+  #
+  # 5.3.2.3.  Percent-Encoding Normalization
+  #
+  # ./.. IRIs should be normalized by decoding any
+  # percent-encoded octet sequence that corresponds to an unreserved
+  # character, as described in section 2.3 of [RFC3986].
+  #
+  if (! Undef->check($self->pct_encoded)) {
+    $rc->{$self->pct_encoded} = sub {
+      my $value = $_[2];
+      #
+      # Global unescape
+      #
+      my $unescaped_ok = 1;
+      my $unescaped;
+      try {
+        my $octets = '';
+        while ($value =~ m/(?<=%)[^%]+/gp) {
+          $octets .= chr(hex(${^MATCH}))
+        }
+        $unescaped = MarpaX::RFC::RFC3629->new($octets)->output
+      } catch {
+        $self->_logger->warnf('%s', $_) for split(/\n/, "$_");
+        $unescaped_ok = 0;
+        return
+      };
+      #
+      # And keep only characters in the unreserved set
+      #
+      if ($unescaped_ok) {
+        my $new_value = '';
+        my $position_in_original_rc = 0;
+        my $unreserved = $self->unreserved;
+        foreach (split('', $unescaped)) {
+          my $length_of_encoded_in_original_rc;
+          try {
+            my $character = $_;
+            my $re_encoded = join('', map { '%' . uc(unpack('H2', $_)) } split(//, encode('UTF-8', $character, Encode::FB_CROAK)));
+            $length_of_encoded_in_original_rc = length($re_encoded);
+          } catch {
+            $self->_logger->warnf('%s', $_) for split(/\n/, "$_");
+          };
+          if (! defined($length_of_encoded_in_original_rc)) {
+            $unescaped_ok = 0;
+            last;
+          }
+          if ($_ =~ $unreserved) {
+            $new_value .= $_;
+          } else {
+            $new_value = substr($value, $position_in_original_rc, $length_of_encoded_in_original_rc);
+          }
+          $position_in_original_rc += $length_of_encoded_in_original_rc;
+        }
+        $value = $new_value if ($unescaped_ok);
+      }
+      $value
+    };
+  }
   $rc
 };
 
