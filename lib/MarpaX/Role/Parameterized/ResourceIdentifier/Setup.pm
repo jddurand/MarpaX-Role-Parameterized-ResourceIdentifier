298f1549c4f272a734c5b283b460fcad72ba2b67
Jean-Damien Durand <jeandamiendurand@free.fr>
2015-11-14 09:25:01 +0100

    ResourceIdentifier/Setup.pm: abs_remote_leading_dots

--- lib/MarpaX/Role/Parameterized/ResourceIdentifier/Setup.pm
+++ lib/MarpaX/Role/Parameterized/ResourceIdentifier/Setup.pm
@@ -11,11 +11,12 @@ use Types::Standard -all;
 
 # AUTHORITY
 
-sub marpa_trace_terminals {($ENV{'MarpaX_RI_MARPA_TRACE_TERMINALS'} // 0) ? 1 : 0   }
-sub marpa_trace_values    {($ENV{'MarpaX_RI_MARPA_TRACE_VALUES'}    // 0) ? 1 : 0   }
-sub uri_compat            { $ENV{'MarpaX_RI_URI_COMPAT'}            // 1            }
-sub plugins_dirname       { $ENV{'MarpaX_RI_PLUGINS_DIRNAME'}       // 'Plugins'    }
-sub can_scheme_methodname { $ENV{'MarpaX_RI_CAN_SCHEME_METHODNAME'} // 'can_scheme' }
+sub marpa_trace_terminals   {($ENV{'MarpaX_RI_MARPA_TRACE_TERMINALS'}   // 0) ? 1 : 0   }
+sub marpa_trace_values      {($ENV{'MarpaX_RI_MARPA_TRACE_VALUES'}      // 0) ? 1 : 0   }
+sub uri_compat              { $ENV{'MarpaX_RI_URI_COMPAT'}              // 1            }
+sub plugins_dirname         { $ENV{'MarpaX_RI_PLUGINS_DIRNAME'}         // 'Plugins'    }
+sub can_scheme_methodname   { $ENV{'MarpaX_RI_CAN_SCHEME_METHODNAME'}   // 'can_scheme' }
+sub abs_remote_leading_dots { $ENV{'MarpaX_RI_ABS_REMOTE_LEADING_DOTS'} // 0            }
 
 with 'MooX::Singleton';
 
