d32cb7d97273a05533a5bcc794f13566656b3ea0
Jean-Damien Durand <jeandamiendurand@free.fr>
2015-11-16 05:47:13 +0100

    dev+: Role/ftp.pm

--- lib/MarpaX/Role/Parameterized/ResourceIdentifier/Role/ftp.pm
+++ lib/MarpaX/Role/Parameterized/ResourceIdentifier/Role/ftp.pm
@@ -11,9 +11,13 @@ package MarpaX::Role::Parameterized::ResourceIdentifier::Role::ftp;
 
 use Carp qw /croak/;
 use Moo::Role;
-
+#
+# Scheme registration
+#
 sub can_scheme { my ($class, $normalized_scheme) = @_; $normalized_scheme eq 'ftp' }
-
+#
+# Scheme validation
+#
 around parse => sub {
   my ($orig, $self) = (shift, shift);
   my @rc = $self->$orig(@_);
@@ -21,5 +25,11 @@ around parse => sub {
   croak 'scheme must be undef or ftp' unless ! defined($normalized_scheme) || $normalized_scheme eq 'ftp';
   @rc
 };
+#
+# Scheme specifics
+#
+#
+# https://tools.ietf.org/html/rfc1738#section-3.2
+#
 
 1;
