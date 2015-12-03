f089b1f166b14443658344437533fb80ce7ec394
Jean-Damien Durand <jeandamiendurand@free.fr>
2015-12-03 14:59:14 +0100

    ftp: [0] issue

--- lib/MarpaX/Role/Parameterized/ResourceIdentifier/Role/ftp.pm
+++ lib/MarpaX/Role/Parameterized/ResourceIdentifier/Role/ftp.pm
@@ -14,22 +14,25 @@ use Moo::Role;
 around default_port => sub { 21 };
 
 around path => sub {
-  my ($orig, $self) = (shift, shift);
-  $self->path_query(@_)
+  my $orig = shift;
+  return ($#_ > 0) ? $_[0]->path_query(@_[1..$#_]) : $_[0]->path_query
 };
 
+#
+# Take care: we want to modify $_[0] eventually
+
 around user => sub {
-  my ($orig, $self) = (shift, shift);
-  my $user = $self->$orig(@_);
+  my $orig = shift;
+  my $user = ($#_ > 0) ? $_[0]->$orig(@_[1..$#_]) : $_[0]->$orig;
   $user = 'anonymous' unless defined $user;
   $user
 };
 
 around password => sub {
-  my ($orig, $self) = (shift, shift);
-  my $password = $self->$orig(@_);
+  my $orig = shift;
+  my $password = ($#_ > 0) ? $_[0]->$orig(@_[1..$#_]) : $_[0]->$orig;
   unless (defined $password) {
-    my $user = $self->user;
+    my $user = $_[0]->user;
     if ($user eq 'anonymous' || $user eq 'ftp') {
       # anonymous ftp login password
       # If there is no ftp anonymous password specified
