81602e60d15c509b6bf4f2f16b56b26c54c1a9c2
Jean-Damien Durand <jeandamiendurand@free.fr>
2015-12-06 19:13:51 +0100

    ldap

---
+++ lib/MarpaX/Role/Parameterized/ResourceIdentifier/Role/ldap.pm
@@ -0,0 +1,14 @@
+use strict;
+use warnings FATAL => 'all';
+
+package MarpaX::Role::Parameterized::ResourceIdentifier::Role::ldap;
+
+# ABSTRACT: Resource Identifier: ldap syntax semantics common overrides role
+
+# VERSION
+
+# AUTHORITY
+
+use Moo::Role;
+
+1;
