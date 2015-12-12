cf6485104a4e6169d496d917590fef6853771c78
Jean-Damien Durand <jeandamiendurand@free.fr>
2015-12-12 08:09:28 +0100

    LDAPString decoding

--- lib/MarpaX/Role/Parameterized/ResourceIdentifier/Role/ldap.pm
+++ lib/MarpaX/Role/Parameterized/ResourceIdentifier/Role/ldap.pm
@@ -13,4 +13,25 @@ use Moo::Role;
 
 around default_port => sub { 389 };
 
+around build_percent_encoding_normalizer => sub {
+  my ($orig, $self) = (shift, shift);
+  my $rc = $self->$orig(@_);
+  #
+  # --------------------------------------------
+  # http://tools.ietf.org/html/rfc3987
+  # --------------------------------------------
+  #
+  # 5.3.2.3.  Percent-Encoding Normalization
+  #
+  # ./.. IRIs should be normalized by decoding any
+  # percent-encoded octet sequence that corresponds to an unreserved
+  # character, as described in section 2.3 of [RFC3986].
+  #
+  if (defined($self->pct_encoded) && defined($self->unreserved)) {
+    my $unreserved = $self->unreserved;
+    $rc->{LDAPString} = sub { $_[0]->percent_decode($_[2], $unreserved) }
+  }
+  $rc
+};
+
 1;
