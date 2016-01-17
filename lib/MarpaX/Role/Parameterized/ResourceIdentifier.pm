13b94c6d93eed3bfad6e05685eecbd6c782fe65d
Jean-Damien Durand <jeandamiendurand@free.fr>
2016-01-17 07:45:11 +0100

    Parameterized/ResourceIdentifier.pm: main package

---
+++ lib/MarpaX/Role/Parameterized/ResourceIdentifier.pm
@@ -0,0 +1,17 @@
+use strict;
+use warnings FATAL => 'all';
+
+package MarpaX::Role::Parameterized::ResourceIdentifier;
+
+# ABSTRACT: Resource Identifier Parameterized role
+
+# VERSION
+
+# AUTHORITY
+
+#
+# This package is doing nothing else but loading the engine
+#
+use MarpaX::Role::Parameterized::ResourceIdentifier::BNF;
+
+1;
